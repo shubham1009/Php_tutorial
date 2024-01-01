21567559185dfb95272398fc4b60aaaf41685e69
shubhamshreesri <websoftnv7@gmail.com>
2024-01-01 22:46:55 +0530

    first

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+print "Jai shree Ram <br> hello world";
+echo "happy new year";
+
+#first comment
+//second way to comment
+/* multi line comment
+here we can 
+comment more than 
+one line */
+
+print "<h2>PHP is Fun!</h2><br>";
+echo "<h2>PHP is Fun!</h2>";
+
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+//print "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+/* While print and echo both serve similar purposes, echo is more commonly used in PHP, and the two can often be used interchangeably. However, it's worth noting that echo is marginally faster and generally more preferred in PHP code. */
+?>
