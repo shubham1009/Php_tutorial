c2ff98343c67987a4a63a9f14420d49af02565e6
shubhamshreesri <websoftnv7@gmail.com>
2024-01-13 13:32:16 +0530

    13 jan

--- firstday.php
+++ firstday.php
@@ -1,11 +1,24 @@
 <?php  
-    $name = "hello everybody";        //Global Variable  
-    function global_var()  
-    {  
-        global $name;  
-        echo "Variable inside the function: ". $name;  
-        echo "</br>";  
-    }  
-    global_var();  
-    echo "Variable outside the function: ". $name;  
+
+$colors = array("red", "green", "blue", "yellow");
+
+var_dump($colors);
+
+foreach ($colors as &$x) {
+  if ($x == "blue") $x = "pink";
+  echo &$x."";
+}
+
+var_dump($colors);
+
+
+    // $name = "hello everybody";        //Global Variable  
+    // function global_var()  
+    // {  
+    //     global $name;  
+    //     echo "Variable inside the function: ". $name;  
+    //     echo "</br>";  
+    // }  
+    // global_var();  
+    // echo "Variable outside the function: ". $name;  
 ?>  
